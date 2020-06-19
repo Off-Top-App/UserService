3c09fd5accba6826813b88a42338a32894c4d202
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-06-19 01:29:35 -0700

    Creating Producer and Consumer so that people can query communicate with this microservice to obtain user data

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
-FROM java:11
-
+FROM adoptopenjdk/openjdk11
+VOLUME [ "/tmp" ]
+EXPOSE 8000
+COPY /target/UserService-0.0.1-SNAPSHOT.jar UserService-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","UserService-0.0.1-SNAPSHOT.jar"]
--- src/main/java/offtop/UserService/UserServiceApplication.java
+++ src/main/java/offtop/UserService/UserServiceApplication.java
@@ -2,8 +2,10 @@ package offtop.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "offtop.UserService")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
